f7ed9b652d47e78b01d0d4590dde3bcbabc5b981
Yifeng Wang <i@ewind.us>
2018-01-03 23:17:22 +0800

    add nested data test

--- test/bumpover/input.js
+++ test/bumpover/input.js
@@ -3,7 +3,9 @@ import { Bumpover } from '../../lib'
 
 test('empty node', t => {
   const input = { children: [] }
+
   const expected = { children: [] }
+
   const rules = [
     {
       match: (node) => false,
@@ -12,8 +14,25 @@ test('empty node', t => {
       })
     }
   ]
+
   const bumper = new Bumpover(rules)
+
   return bumper
     .bump(input)
     .then(actual => t.deepEqual(actual, expected))
+})
+
+test('undefined node', async t => {
+  const rules = [
+    {
+      match: (node) => false,
+      update: (node) => new Promise((resolve, reject) => {
+        resolve({ node })
+      })
+    }
+  ]
+
+  const bumper = new Bumpover(rules)
+
+  await t.throws(bumper.bump())
 })
--- test/bumpover/rule.js
+++ test/bumpover/rule.js
@@ -3,9 +3,13 @@ import { Bumpover } from '../../lib'
 
 test('empty rule', t => {
   const input = { children: [] }
+
   const expected = { children: [] }
+
   const rules = []
+
   const bumper = new Bumpover(rules)
+
   return bumper
     .bump(input)
     .then(actual => t.deepEqual(actual, expected))
@@ -15,4 +19,61 @@ test('undefined rule', async t => {
   const input = { children: [] }
   const bumper = new Bumpover()
   await t.notThrows(bumper.bump(input))
+})
+
+test('nested input, single rule', t => {
+  const input = {
+    id: 1,
+    children: [
+      {
+        id: 2,
+        children: [
+          {
+            id: 3,
+            children: [
+              { id: 4, children: [] },
+              { id: 5, children: [] },
+              { id: 6, children: [] }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+
+  const expected = {
+    id: 2,
+    children: [
+      {
+        id: 3,
+        children: [
+          {
+            id: 4,
+            children: [
+              { id: 5, children: [] },
+              { id: 6, children: [] },
+              { id: 7, children: [] }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+
+  const rules = [
+    {
+      match: (node) => true,
+      update: (node) => new Promise((resolve, reject) => {
+        resolve({
+          node: { ...node, id: node.id + 1 }
+        })
+      })
+    }
+  ]
+
+  const bumper = new Bumpover(rules)
+
+  return bumper
+    .bump(input)
+    .then(actual => t.deepEqual(actual, expected))
 })
