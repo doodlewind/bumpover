dc63b6dbfc28828a5df28bd5b8bda2a8db9c721a
Yifeng Wang <i@ewind.us>
2018-01-04 11:31:35 +0800

    add tests for invalid actions

--- test/bumpover/options.js
+++ test/bumpover/options.js
@@ -137,3 +137,71 @@ test('stop action on node', t => {
 
   return bumper.bump(input).then(actual => t.deepEqual(actual, expected))
 })
+
+test('invalid action on root', async t => {
+  const input = {
+    name: 'div',
+    props: {},
+    children: [
+      {
+        name: 'span',
+        props: {},
+        children: [
+          { name: 'span', props: {}, children: [] },
+          { name: 'small', props: {}, children: [] },
+          { name: 'span', props: {}, children: [] }
+        ]
+      }
+    ]
+  }
+
+  const rules = [
+    {
+      match: ({ name }) => name === 'div',
+      update: (node) => new Promise((resolve, reject) => {
+        resolve({
+          action: 'nothing',
+          node: { ...node, props: { background: 'red' } }
+        })
+      })
+    }
+  ]
+
+  const bumper = new Bumpover(rules)
+
+  await t.throws(bumper.bump(input))
+})
+
+test('invalid action on node', async t => {
+  const input = {
+    name: 'div',
+    props: {},
+    children: [
+      {
+        name: 'span',
+        props: {},
+        children: [
+          { name: 'span', props: {}, children: [] },
+          { name: 'small', props: {}, children: [] },
+          { name: 'span', props: {}, children: [] }
+        ]
+      }
+    ]
+  }
+
+  const rules = [
+    {
+      match: ({ name }) => name === 'span',
+      update: (node) => new Promise((resolve, reject) => {
+        resolve({
+          action: 'nothing',
+          node: { ...node, props: { fontSize: '16px' } }
+        })
+      })
+    }
+  ]
+
+  const bumper = new Bumpover(rules)
+
+  await t.throws(bumper.bump(input))
+})
