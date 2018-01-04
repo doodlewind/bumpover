526d6c06baca6f36d5ff96d2e6c6edf02255a087
Yifeng Wang <i@ewind.us>
2018-01-04 16:19:43 +0800

    add test for async rule

--- test/bumpover/rule.js
+++ test/bumpover/rule.js
@@ -76,6 +76,63 @@ test('nested input, single rule', t => {
   return bumper.bump(input).then(actual => t.deepEqual(actual, expected))
 })
 
+test('nest input, async rule', async t => {
+  const input = {
+    id: 1,
+    children: [
+      {
+        id: 2,
+        children: [
+          {
+            id: 3,
+            children: [
+              { id: 4, children: [] },
+              { id: 5, children: [] },
+              { id: 6, children: [] }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+
+  const expected = {
+    id: 2,
+    children: [
+      {
+        id: 3,
+        children: [
+          {
+            id: 4,
+            children: [
+              { id: 5, children: [] },
+              { id: 6, children: [] },
+              { id: 7, children: [] }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+
+  const rules = [
+    {
+      match: (node) => true,
+      update: (node) => new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            node: { ...node, id: node.id + 1 }
+          })
+        }, 10)
+      })
+    }
+  ]
+
+  const bumper = new Bumpover(rules)
+
+  return bumper.bump(input).then(actual => t.deepEqual(actual, expected))
+})
+
 test('nested input, multi rules', t => {
   const input = {
     name: 'div',
